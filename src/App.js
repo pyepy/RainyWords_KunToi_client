2bce3113ffd47e20fd59291ac0d025e96be921dc
pyepy <123175407+pyepy@users.noreply.github.com>
2023-10-03 09:40:25 +0700

    start app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import { useEffect, useState} from "react";
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  //room state
+  const [room, setRoom] = useState("");
+
+  //message state
+  const [message, setMessage] = useState("");
+  const [messageRecieved, setMessageRecieved] = useState("");
+  
+  const joinRoom = () => {
+    if(room !== ""){
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", {message, room})
+  };
+
+  useEffect(() =>{
+    socket.on("recieve_message", (data) =>{
+      setMessageRecieved(data.message);
+    })
+  }, [socket])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        placeholder='room number' 
+        onChange={(event) =>{
+          setRoom(event.target.value);
+        }}
+      ></input>
+      <button onClick={joinRoom}>join room</button>
+
+      <input 
+        placeholder='message' 
+        onChange={(event) =>{
+          setMessage(event.target.value);
+        }}
+      ></input>
+      <button onClick={sendMessage}>send message</button>
+      {/* <h1>My room: {room}</h1> */}
+      <h1>Message:</h1>
+      <h1>{messageRecieved}</h1>
     </div>
   );
 }

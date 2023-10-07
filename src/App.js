e10490eba887d31a8619ec6d6fc69789659feb08
Napat-Lee <frostingpotato@gmail.com>
2023-10-07 21:46:08 +0700

    C: username & room
    + Add username (before doing anything else)
    + Seperate page for start and other
    + Online Counter (connect/disconnect)
    . Can only be in one room at a time
    . Install some npm stuff (reacts-scripts, socket-client)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,92 @@
 import './App.css';
 import io from "socket.io-client";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   //room state
-  const [room, setRoom] = useState("");
+  const [oldRoom, setOldRoom] = useState(""); //current room
+  const [newRoom, setNewRoom] = useState(""); //room code in input
+
+   //add username
+  const [name,setName] = useState("");  
+  //no. of online user
+  const [count,setCount] = useState("");
 
   //message state
   const [message, setMessage] = useState("");
   const [messageRecieved, setMessageRecieved] = useState("");
   
   const joinRoom = () => {
-    if(room !== ""){
-      socket.emit("join_room", room);
+    if(newRoom !== ""){
+      socket.emit("join_room", {oldRoom, newRoom});
     }
   };
 
+  const addName = () => {   //register name, change to room 1
+    socket.emit("add_name", {name, oldRoom, newRoom})
+  };
+
   const sendMessage = () => {
-    socket.emit("send_message", {message, room})
+    socket.emit("send_message", {message, oldRoom})
   };
 
   useEffect(() =>{
     socket.on("recieve_message", (data) =>{
       setMessageRecieved(data.message);
-    })
+    });
+
+    socket.on("ack_room", (data) => { //set oldRoom = newRoom
+      setOldRoom(data.newRoom)
+    });
+
+    socket.on("online_no", (data) => {  //check no. of online players
+      setCount(data);
+    });
   }, [socket])
 
-  return (
-    <div className="App">
-      <input 
-        placeholder='room number' 
-        onChange={(event) =>{
-          setRoom(event.target.value);
+  if (oldRoom == "") {  //first entering the url
+    return (
+      <div className="Welcome"> 
+        <h3>Online Players: {count}</h3> 
+        <h1>Welcome to KunToi Game</h1>  
+        <input 
+        placeholder = "Enter Your Username" 
+        onChange={(event) => {
+          setName(event.target.value);
+          setNewRoom(1);
         }}
-      ></input>
-      <button onClick={joinRoom}>join room</button>
-
-      <input 
-        placeholder='message' 
-        onChange={(event) =>{
-          setMessage(event.target.value);
-        }}
-      ></input>
-      <button onClick={sendMessage}>send message</button>
-      {/* <h1>My room: {room}</h1> */}
-      <h1>Message:</h1>
-      <h1>{messageRecieved}</h1>
-    </div>
-  );
+      />
+      <button onClick={addName}> Confirm</button>  
+      </div>
+    );
+  } else {    //after entering username
+    return (
+      <div className="App">
+        <h3>Online Players: {count}</h3> 
+        <h1>Welcome, {name}</h1>
+        <h3> </h3>
+        <input 
+          placeholder='room number' 
+          onChange={(event) =>{
+            setNewRoom(event.target.value);
+          }}
+        ></input>
+        <button onClick={joinRoom}>join room</button>
+  
+        <input 
+          placeholder='message' 
+          onChange={(event) =>{
+            setMessage(event.target.value);
+          }}
+        ></input>
+        <button onClick={sendMessage}>send message</button>
+        <h1>My room: {oldRoom}</h1>
+        <h1>Message:</h1>
+        <h1>{messageRecieved}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;

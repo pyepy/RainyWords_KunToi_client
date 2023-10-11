14c780b4da3d9acabdefb65c11f4ea4090bc9a1f
jeklao69420 <97685075+jeklao69420@users.noreply.github.com>
2023-10-11 11:30:11 +0700

    คุณหรีดแม่ง cringe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Game from './game';
 import './App.css';
-
 import { SendMessage } from './components/SendMessage';
 import { PlayerCount } from './components/PlayerCount';
 import { AddUsername } from './components/AddUsername';
 import { RandomWord } from './components/RandomWord';
 
 function App() {
+  const handleStartGame = () => {
+    window.location.href="/game" // Refresh the page
+  };
 
-  return (        //display
-    <div className="App">
-      <PlayerCount/>
-      <AddUsername/>
-      <SendMessage/>
-      <RandomWord/>
-    </div>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/game" element={<Game />} />
+          {/* Other routes can be added here if needed */}
+        </Routes>
+        
+        {window.location.pathname !== '/game' && (
+          <>
+            <PlayerCount />
+            <AddUsername />
+            <SendMessage />
+            <RandomWord />
+            <Link to="/game">
+              <button onClick={handleStartGame}>Start the fucking game</button>
+            </Link>
+          </>
+        )}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Game = () => {
+  // Add your game content here
+  return (
+    <div>
+      <h1>Game Page</h1>
+      <p>Welcome to the game! Your game content goes here.</p>
+      {/* Add more game-related components or elements as needed. */}
+    </div>
+  );
+};
+
+export default Game;

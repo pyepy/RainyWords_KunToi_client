7cb580273d5782c1f98585ab4cb2d7c90f2ac3ba
jeklao69420 <97685075+jeklao69420@users.noreply.github.com>
2023-10-11 13:26:27 +0700

    我们不一样

--- src/game.js
+++ src/game.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './App.css';
+import { ReactP5Wrapper } from 'react-p5-wrapper'; 
+import sketch from './sketch'
 
 const Game = () => {
   // Add your game content here
   return (
-    <div>
+    <div className="App">
       <h1>Game Page</h1>
       <p>Welcome to the game! Your game content goes here.</p>
+      <header className="App-header">
+        <>
+          <ReactP5Wrapper sketch={sketch} />
+        </>
+      </header>
+      
       {/* Add more game-related components or elements as needed. */}
     </div>
   );

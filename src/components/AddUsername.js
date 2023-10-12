8fbf036b03b1ba2943354205bf9237c42933a0ff
Napat-Lee <frostingpotato@gmail.com>
2023-10-12 22:39:05 +0700

    C: AddUsername v2.0
    + assigning name is now a server-side action
    + list of all online players from server (idk what to do with it but we have it)

--- src/components/AddUsername.js
+++ src/components/AddUsername.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { socket } from '../socket'
 
 export function AddUsername() {
   const [tempName,setTempName] = useState("");    //name written in input
   const [name,setName] = useState("");            //name sent
+  const [nameList,setNameList] = useState("");            
 
-  const addName = () => {
-    if (tempName === "") {
-      var x = (Math.floor(Math.random() * 900)+100)   //assign random ID
-      setName("KunToi#"+x);                           //default name
-    } else {
-      setName(tempName);
-    }
-    socket.emit("add_name", name)                     //no func yet
+  const addName = () => {       
+    socket.emit("assign_name",tempName);          //assign server-side
   }
 
+  useEffect(() => {
+    socket.on("ack_name", (data) => {  //rcv name and namelist from server
+        setName(data.name);
+        setNameList(data.namelist);
+    });
+
+  });
 
   if (name == "") {      //if no name assigned yet
     return(   //display

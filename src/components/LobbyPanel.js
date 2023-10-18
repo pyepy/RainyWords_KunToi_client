32313ae5f4860609036f90cac12174532c2bbdf5
pyepy <123175407+pyepy@users.noreply.github.com>
2023-10-18 18:58:26 +0700

    send info back to lobby in work

--- src/components/LobbyPanel.js
+++ src/components/LobbyPanel.js
@@ -3,24 +3,39 @@ import { socket } from '../socket'
 import playerIcon from '../images/playerIcon.png';
 
 export function LobbyPanel() {
+    socket.emit('request_room_info');
+
+    const [gameMode, setGameMode] = useState('Game');
+    const [roomNo, setRoomNo] = useState('00000');
+    const [playerOne, setPlayerOne] = useState('Waiting');
+    const [playerTwo, setPlayerTwo] = useState('Waiting');
+    const [playerInLobby, setPlayerInLobby] = useState(0);
+
+    useEffect(() => {
+        socket.on("giveRoomInfo", (data) => {  //rcv name and namelist from server
+            console.log(data)
+        })
+    
+      });
+
 
     return (
         <div className="lobbyContainer">
             <div className='gameMode'>
-                Classic Mode
+                {gameMode} Mode
             </div>
             <div className='lobbyCount'>
-                <span>player : 1/2</span>
-                <span className='waiting'>waiting... </span>
+                <span>Room No. {roomNo}</span>
+                <span className='waiting'>player : {playerInLobby}/2 </span>
             </div>
             <div className='players'>
                 <div className='player'>
                     <img className="playerIcon" src={playerIcon}/>
-                     player 1
+                     {playerOne}
                 </div>
                 <div className='player'>
                     <img className="playerIcon" src={playerIcon}/>
-                    player 2
+                    {playerTwo}
                 </div>
             </div>
             <button className='startGame'>Start Game</button>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,22 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { socket } from '../socket'
 import { NavItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export function Menu(props) {
   const Create_room = 'Create room';
   const Join_room = 'Join room';
+
+  const navigate = useNavigate();
 
   const requestCreateRoom = (gameMode) => {   
     socket.emit("request_create_room", {gameMode});          //assign server-side
   }
 
   useEffect(() => {
-    socket.on("roomCreated", (data) => {  //rcv name and namelist from server
-
+    const handleRoomCreated = (data) => {
       console.log(data.myRoom);
-    })
+      // Use the history object for navigation, assuming you're using React Router
+      //window.location.href = '/play';
+      navigate('/play');
+    };
+  
+    // Add the event listener when the component mounts
+    socket.on("roomCreated", handleRoomCreated);
+  
+    // Remove the event listener when the component unmounts to avoid duplicates
+    return () => {
+      socket.off("roomCreated", handleRoomCreated);
+    };
+  }, []);
 
-  });
+  // window.addEventListener("beforeunload", () => {
+  //   socket.disconnect();
+  // });
 
   return (
     <div className="Menu">
@@ -32,7 +48,7 @@ export function Menu(props) {
                     Classic
                   </div>
                 {/* </NavItem> */}
-                <div className="room">
+                <div className="room" onClick={() => requestCreateRoom("Arcade")}>
                   Arcade
                 </div>
               </>
